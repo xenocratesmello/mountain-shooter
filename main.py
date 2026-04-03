275fc761a1ad702503f85cc06ce8e9adf640ce4c
Xenócrates Mello <61846361+xenocratesmello@users.noreply.github.com>
2026-04-03 17:11:49 -0300

    Create the PyGame Window and update the .gitignore.

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-def main():
-    print("Hello from mountainshooter!")
+import pygame
 
+# To use the packege it must be initialized.
+print('Setup Start')
+pygame.init()
 
-if __name__ == "__main__":
-    main()
+# Create the game's window
+window = pygame.display.set_mode(size=(600, 480))
+print('Setup End')
+
+print('Loop Start')
+while True:
+    # Check for all events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()  # Close window
+            quit()  # End pygame
